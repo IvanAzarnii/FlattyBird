9303a127fae05d1af1ca224c20e54bcc38b4a1be
Ivan <TheWinterManIsHere@gmail.com>
2016-12-18 12:50:02 +0300

    Initial Commit

---
+++ Assets/scripts/OnHitEdges.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHitEdges : MonoBehaviour {
+
+    BoxCollider2D collider;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/init.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class init : MonoBehaviour
+{
+    void OnStart()
+    {
+        Time.timeScale = 0;
+    }
+    void update()
+    {
+
+    }
+}

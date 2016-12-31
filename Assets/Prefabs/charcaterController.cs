56252e5e24df12b3bfffa6fca20e8d17f5bcc11b
Ivan <TheWinterManIsHere@gmail.com>
2016-12-31 22:20:30 +0300

    TODO:: GUI, MULTI-SCREEN SUPPORT

---
+++ Assets/Prefabs/charcaterController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+public class charcaterController : MonoBehaviour {
+
+    Rigidbody2D body;
+    Vector2 jump = new Vector2(0.0f, 800.0f);
+    void Start () {
+
+        body = this.GetComponent<Rigidbody2D>();
+        
+	}
+	
+	void Update () {
+        OnKey();
+                
+	}
+    void OnKey()
+    {
+        AudioSource audio = this.GetComponent<AudioSource>();
+        if (/*Input.GetTouch(0).phase == TouchPhase.Stationary ||*/ Input.GetKeyDown(KeyCode.Space))
+        {
+            body.velocity = Vector2.zero;
+            body.AddForce(jump);
+            audio.Play();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(coll.gameObject);
+        Sce
+    }
+    /*void OnDestroy()
+    {
+        Time.timeScale = 0; 
+    }*/
+}
---
+++ Assets/Prefabs/obstacleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obstacleController : MonoBehaviour {
+
+    Vector2 velocity = new Vector2(-9.0f, 0.0f);
+    Rigidbody2D rg2d;
+    float range;
+
+
+    void Start()
+    {
+        rg2d = this.GetComponent<Rigidbody2D>();
+        rg2d.velocity = velocity;
+        InvokeRepeating("CreateObstacles", 0.8f, 0.8f);
+        range = Random.Range(-5.0f, 5.0f);
+    }
+    void CreateObstacles()
+    {
+        Instantiate(this, new Vector3(35, 5 + range, 0), Quaternion.identity);
+        Destroy(GetComponent<obstacleController>());
+
+    }
+    void Update()
+    {
+
+    }
+}
